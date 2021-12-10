c34450e42d883b2da9e2cd99829f458af3de318e
Brian "Beej Jorgensen" Hall <beej@beej.us>
2021-12-10 13:22:20 -0800

    added

---
+++ source/examples/alignof_demo.c
@@ -0,0 +1,22 @@
+#include <stdalign.h>
+#include <stdio.h>     // for printf()
+#include <stddef.h>    // for max_align_t
+
+struct t {
+    int a;
+    char b;
+    float c;
+};
+
+int main(void)
+{
+    printf("char       : %zu\n", alignof(char));
+    printf("short      : %zu\n", alignof(short));
+    printf("int        : %zu\n", alignof(int));
+    printf("long       : %zu\n", alignof(long));
+    printf("long long  : %zu\n", alignof(long long));
+    printf("double     : %zu\n", alignof(double));
+    printf("long double: %zu\n", alignof(long double));
+    printf("struct t   : %zu\n", alignof(long double));
+    printf("max_align_t: %zu\n", alignof(max_align_t));
+}
---
+++ source/examples/funcspec_noreturn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+
+noreturn void foo(void) // This function should never return!
+{
+    printf("Happy days\n");
+
+    exit(1);            // And it doesn't return--it exits here!
+}
+
+int main(void)
+{
+    foo();
+}
