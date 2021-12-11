784e3f2fee81c7f2fd55259b418b71a40dfaaa4d
Brian "Beej Jorgensen" Hall <beej@beej.us>
2021-12-11 15:01:57 -0800

    add man example config

--- bin/preproc_config.py
+++ bin/preproc_config.py
@@ -1,3 +1,3 @@
-EXAMPLE_URL = "https://beej.us/guide/bgc/examples/"
+EXAMPLE_URL = "https://beej.us/guide/bgc/source/examples/"
 WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
 MAN_URL = "https://man.archlinux.org/man/"
