c4835fb87f65553e9ed8e69055cfbd71323af998
Brian 'Beej Jorgensen' Hall <beej@beej.us>
2021-12-11 18:53:24 -0800

    add exercise solution links

--- bin/preproc_config.py
+++ bin/preproc_config.py
@@ -1,3 +1,4 @@
 EXAMPLE_URL = "https://beej.us/guide/bgc/source/examples/"
+EXERCISE_URL = "https://beej.us/guide/bgc/source/exercises/"
 WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
 MAN_URL = "https://man.archlinux.org/man/"
